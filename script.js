ace4b03870d666ffe63aad424f3c89c8c1cfd55a
Olexander Krasulia <krasulazr@gmail.com>
2022-12-18 00:35:09 +0200

    DZ10 corected

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// function pow(num, degree) {
+//     if (degree === 1) {
+//       console.log(num)
+//     } else {
+//       console.log(degree * Math.pow(num ** degree));
+//     }
+//   }
+//   console.log(Math.pow(2,8));
+
 function pow(num, degree) {
-    if (degree === 1) {
-      console.log(num)
-    } else {
-      console.log(degree * Math.pow(num ** degree));
-    }
+  if(degree === 1) {
+      return num;
+  }else{
+      return num * pow(num, degree - 1);
   }
-  console.log(Math.pow(2,8));
+}
+console.log(pow(3,2))
